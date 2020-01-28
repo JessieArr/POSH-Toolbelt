0a4a19924ee2034517d31ce4a7d61e09c56a753b
ShaWn <smorrison1986@gmail.com>
2020-01-27 22:38:25 -0600

    Hello, world!

---
+++ POSH-Toolbelt/POSH-Toolbelt/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POSH_Toolbelt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Runspace rs;
+        public MainWindow()
+        {
+            InitializeComponent();
+            var rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var scriptTask = RunScript(Script.Text);
+            scriptTask.ContinueWith(x =>
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    foreach (var line in x.Result)
+                    {
+                        Output.Content += line + Environment.NewLine;
+                    }
+                });                
+            });
+        }
+
+        private async Task<PSDataCollection<PSObject>> RunScript(string script)
+        {
+            using (PowerShell ps = PowerShell.Create())
+            {
+                ps.AddScript(script);
+                ps.Runspace = rs;
+                return await ps.InvokeAsync();
+            }
+        }
+    }
+}
